9c6c3a3de1a3e745c478ba5098f28a03e5038a2e
ALex <alexanderstubmichelsen@gmail.com>
2024-10-21 12:31:39 +0200

    now only loades the 6 tables neede from fresh

--- load-ogr.sh
+++ load-ogr.sh
@@ -1,4 +1,47 @@
 #!/bin/bash
 URL="https://geoserver.plandata.dk/geoserver/wfs?servicename=wfs&request=getcapabilities&service=wfs"
 
-ogr2ogr -f "PostgreSQL" PG:"dbname=crawler" -lco SCHEMA=plandata -skipfailures $URL
+SCHEMA=plandata_test
+
+ogr2ogr -f "PostgreSQL" PG:"dbname=crawler"\
+ $URL\
+ -nln theme_pdk_kommuneplan_oversigt_forslag_v "pdk:theme_pdk_kommuneplan_oversigt_forslag_v"\
+ -lco SCHEMA=$SCHEMA\
+ -lco OVERWRITE=YES\
+ -skipfailures
+
+ogr2ogr -f "PostgreSQL" PG:"dbname=crawler"\
+ $URL\
+ -nln theme_pdk_kommuneplan_oversigt_vedtaget_v "pdk:theme_pdk_kommuneplan_oversigt_vedtaget_v"\
+ -lco SCHEMA=$SCHEMA\
+ -lco OVERWRITE=YES\
+ -skipfailures
+
+ogr2ogr -f "PostgreSQL" PG:"dbname=crawler"\
+ $URL\
+ -nln theme_pdk_lokalplan_forslag_v "pdk:theme_pdk_lokalplan_forslag_v"\
+ -lco SCHEMA=$SCHEMA\
+ -lco OVERWRITE=YES\
+ -skipfailures
+
+ogr2ogr -f "PostgreSQL" PG:"dbname=crawler"\
+ $URL\
+ -nln theme_pdk_lokalplan_vedtaget_v "pdk:theme_pdk_lokalplan_vedtaget_v"\
+ -lco SCHEMA=$SCHEMA\
+ -lco OVERWRITE=YES\
+ -skipfailures
+
+ogr2ogr -f "PostgreSQL" PG:"dbname=crawler"\
+ $URL\
+ -nln theme_pdk_zonekort_samlet_v "pdk:theme_pdk_zonekort_samlet_v"\
+ -lco SCHEMA=$SCHEMA\
+ -lco OVERWRITE=YES\
+ -skipfailures
+ogr2ogr -f "PostgreSQL" PG:"dbname=crawler"\
+ $URL\
+ -nln theme_pdk_zonekort_v "pdk:theme_pdk_zonekort_v"\
+ -lco SCHEMA=$SCHEMA\
+ -lco OVERWRITE=YES\
+ -skipfailures
+
+
