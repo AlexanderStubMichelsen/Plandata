a8be88d4f81144630a77b1fe0c8e338b37e8d5fc
ALex <alexanderstubmichelsen@gmail.com>
2024-11-07 13:11:40 +0100

    updated the create schirpt to use the schema from .env

--- create_plans_tables.sql
+++ create_plans_tables.sql
@@ -1,6 +1,6 @@
+\set env_schema `echo "$SCHEMA"`
 
--- komuneplan oversigt forslag
-CREATE OR REPLACE VIEW plandata.komuneplan_oversigt_forslag_uden_geometri_v
+CREATE OR REPLACE VIEW :env_schema.komuneplan_oversigt_forslag_uden_geometri_v
 AS SELECT ogc_fid,
     gml_id,
     oid_,
@@ -23,10 +23,10 @@ AS SELECT ogc_fid,
     glkomnr,
     kommunenavn,
     glkomnavn
-   FROM plandata.theme_pdk_kommuneplan_oversigt_forslag_v;
+   FROM :env_schema.theme_pdk_kommuneplan_oversigt_forslag_v;
 
 -- komuneplan oversigt vedtaget
-CREATE OR REPLACE VIEW plandata.komuneplan_oversigt_vedtaget_uden_geometri_v
+CREATE OR REPLACE VIEW :env_schema.komuneplan_oversigt_vedtaget_uden_geometri_v
 AS SELECT ogc_fid,
     gml_id,
     oid_,
@@ -49,10 +49,10 @@ AS SELECT ogc_fid,
     glkomnr,
     kommunenavn,
     glkomnavn
-   FROM plandata.theme_pdk_kommuneplan_oversigt_vedtaget_v;
+   FROM :env_schema.theme_pdk_kommuneplan_oversigt_vedtaget_v;
   
 -- address only view
-CREATE OR REPLACE VIEW plandata.address_only_buildings
+CREATE OR REPLACE VIEW :env_schema.address_only_buildings
 AS SELECT adresse,
     validitet,
     enhedadressebetegnelse,
@@ -87,39 +87,39 @@ AS SELECT adresse,
     koord_4326_y,
     koord_25832,
     koord_4326
-   FROM adresse
+   FROM public.adresse
   WHERE etagebetegnelse IS NULL;
 
 -- Lokalplan
-DROP TABLE IF EXISTS plandata.lokalplan_for_adresse;
-CREATE TABLE plandata.lokalplan_for_adresse as
-    SELECT a.adresse, a.adgangsadressebetegnelse, l.plannavn plan_navn, l.doklink plandata_link, l.datoforsl dato_forslået, l.datovedt dato_vedtaget, l.datoaflyst dato_aflyst, l.datoikraft dato_i_kraft, l.datostart dato_start, l.datoslut dato_slut, l.datoopdt dato_opdateret
-        FROM plandata.theme_pdk_lokalplan_vedtaget_v l
-        JOIN plandata.address_only_buildings a on a.kommune = l.komnr
+DROP TABLE IF EXISTS :env_schema.lokalplan_for_adresse ;
+CREATE TABLE :env_schema.lokalplan_for_adresse as
+    SELECT a.adresse, a.adgangsadressebetegnelse, l.id plan_id, l.plannavn plan_navn, l.doklink dok_link, l.datoforsl dato_forslået, l.datovedt dato_vedtaget, l.datoaflyst dato_aflyst, l.datoikraft dato_i_kraft, l.datostart dato_start, l.datoslut dato_slut, l.datoopdt dato_opdateret
+        FROM :env_schema.theme_pdk_lokalplan_vedtaget_v l
+        JOIN public.address_only_buildings a on a.kommune = l.komnr
         WHERE st_isvalid(l.geometri) and st_within(a.koord_25832, l.geometri);
-INSERT INTO plandata.lokalplan_for_adresse
-    SELECT a.adresse, l.doklink, false vedtaget
-    FROM plandata.theme_pdk_lokalplan_forslag_v l
-    JOIN plandata.address_only_buildings a on a.kommune = l.komnr
-    WHERE st_isvalid(l.geometri) and st_within(a.koord_25832, l.geometri)
+INSERT INTO :env_schema.lokalplan_for_adresse
+    SELECT a.adresse, a.adgangsadressebetegnelse, l.id plan_id, l.plannavn plan_navn, l.doklink dok_link, l.datoforsl dato_forslået, l.datovedt dato_vedtaget, l.datoaflyst dato_aflyst, l.datoikraft dato_i_kraft, l.datostart dato_start, l.datoslut dato_slut, l.datoopdt dato_opdateret
+    FROM :env_schema.theme_pdk_lokalplan_forslag_v l
+    JOIN public.address_only_buildings a on a.kommune = l.komnr
+    WHERE st_isvalid(l.geometri) and st_within(a.koord_25832, l.geometri);
 
 -- Komuneplan
 
-DROP TABLE if EXISTS plandata.komuneplan_for_adresse;
-CREATE TABLE plandata.komuneplan_for_adresse as
-    SELECT a.adresse, a.adgangsadressebetegnelse, k.plannavn plan_navn, k.doklink plandata_link, k.datoforsl dato_forslået, k.datovedt dato_vedtaget, k.datoaflyst dato_aflyst, k.datoikraft dato_i_kraft, k.datostart dato_start, k.datoslut dato_slut, k.datoopdt dato_opdateret
-    FROM plandata.address_only_buildings AS a
-    JOIN plandata.komuneplan_oversigt_vedtaget_uden_geometri_v AS k ON a.kommune = k.komnr;
-INSERT INTO plandata.komuneplan_for_adresse
-    SELECT a.adresse, k.doklink, false vedtaget
-    FROM plandata.address_only_buildings AS a
-    JOIN plandata.komuneplan_oversigt_forslag_uden_geometri_v AS k ON a.kommune = k.komnr;
+DROP TABLE if EXISTS :env_schema.komuneplan_for_adresse;
+CREATE TABLE :env_schema.komuneplan_for_adresse as
+    SELECT a.adresse, a.adgangsadressebetegnelse, k.id plan_id, k.plannavn plan_navn, k.doklink dok_link, k.datoforsl dato_forslået, k.datovedt dato_vedtaget, k.datoaflyst dato_aflyst, k.datoikraft dato_i_kraft, k.datostart dato_start, k.datoslut dato_slut, k.datoopdt dato_opdateret
+    FROM public.address_only_buildings AS a
+    JOIN :env_schema.komuneplan_oversigt_vedtaget_uden_geometri_v AS k ON a.kommune = k.komnr;
+INSERT INTO :env_schema.komuneplan_for_adresse
+    SELECT a.adresse, a.adgangsadressebetegnelse, k.id plan_id, k.plannavn plan_navn, k.doklink dok_link, k.datoforsl dato_forslået, k.datovedt dato_vedtaget, k.datoaflyst dato_aflyst, k.datoikraft dato_i_kraft, k.datostart dato_start, k.datoslut dato_slut, k.datoopdt dato_opdateret
+    FROM public.address_only_buildings AS a
+    JOIN :env_schema.komuneplan_oversigt_forslag_uden_geometri_v AS k ON a.kommune = k.komnr;
 
 -- Zonekort
-DROP TABLE if EXISTS plandata.Zonekort_for_adresse;
-CREATE TABLE plandata.Zonekort_for_adresse as
+DROP TABLE if EXISTS :env_schema.Zonekort_for_adresse;
+CREATE TABLE :env_schema.Zonekort_for_adresse as
     SELECT a.adresse, a.adgangsadressebetegnelse, z.zone, z.zonestatus
-    FROM plandata.address_only_buildings AS a
-    JOIN plandata.theme_pdk_zonekort_v AS z ON a.kommune = z.komnr
-    WHERE st_isvalid(z.geometri) and st_within(a.koord_25832, z.geometri)
+    FROM public.address_only_buildings AS a
+    JOIN :env_schema.theme_pdk_zonekort_v AS z ON a.kommune = z.komnr
+    WHERE st_isvalid(z.geometri) and st_within(a.koord_25832, z.geometri);
 
